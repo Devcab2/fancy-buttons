d532e8947d57607a22ea793310df89c46b5127fe
Chris Bowyer <100442597+Devcab2@users.noreply.github.com>
2022-09-12 14:58:04 -0600

    configure react buttons + implemement dark mode

---
+++ src/components/LightSwitchButton.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function LightSwitchButton(props){
+  const {light, switchLight} = props;
+  const handleClick = () => switchLight();
+  return (
+    <button className="LightSwitchButton" onClick={handleClick}>
+      {light === "on" && (
+        <span>
+          <i>💡</i> I'm on!
+        </span>
+      )}
+      {light === "off" && (
+        <span>
+          <i>💡</i> I'm off!
+        </span>
+      )}
+    </button>
+  );
+}
+
+export default LightSwitchButton;
